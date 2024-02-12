9546710493776b562602904ed64af17d52015f5c
Dustin Schaerer <dustinschaerer@gmail.com>
2024-02-12 11:39:09 -0800

    Wait until ActiveSupport on_load to include DdbController

--- lib/userstamp.rb
+++ lib/userstamp.rb
@@ -45,4 +45,6 @@ module Ddb
   end
 end
 
-ActionController::Base.send(:include, Ddb::Controller) if defined?(ActionController)
+ActiveSupport.on_load(:action_controller) do
+  ActionController::Base.send(:include, Ddb::Controller) if defined?(ActionController)
+end
